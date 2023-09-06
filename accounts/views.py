825cc607d20cffa2827ab83602150f6514e6b280
Kyle <bossertkt@gmail.com>
2023-09-05 23:18:14 -0700

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -29,3 +30,21 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            login(request, user)
+            return redirect("list_projects")
+    else:
+        form = SignupForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
