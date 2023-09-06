daa453bee57a369db14d50e12271ff683e9438d1
Kyle <bossertkt@gmail.com>
2023-09-05 21:53:57 -0700

    Feature 7 Complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -20,11 +20,12 @@ from django.shortcuts import redirect
 
 
 def redirect_to_home(request):
-    return redirect("home_page")
+    return redirect("list_projects")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", redirect_to_home, name="home"),
 ]
